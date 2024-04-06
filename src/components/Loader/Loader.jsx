e1c9ad58975adf9f4103273428d6202331ffc5a9
ArtemBlyshchyk <artem.bl31@gmail.com>
2024-04-06 16:54:59 +0300

    Added country info

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,3 +1,10 @@
+import { BeatLoader } from 'react-spinners';
+import styles from './Loader.module.css';
+
 export const Loader = () => {
-  return <h2>Loader</h2>;
+  return (
+    <div className={styles.backdrop}>
+      <BeatLoader color="#36d7b7" />;
+    </div>
+  );
 };
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,35 @@
-import { Container, Heading, Section } from 'components';
+import { Container, CountryInfo, Heading, Loader, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchCountry } from 'service/countryApi';
 
 export const Country = () => {
+  const { countryId } = useParams();
+  const [country, setCountry] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    if (!countryId) return;
+    const getCountry = async () => {
+      setLoading(true);
+      try {
+        const country = await fetchCountry(countryId);
+        setCountry(country);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getCountry();
+  }, [countryId]);
+
   return (
     <Section>
       <Container>
-        <Heading title="SearchCountry" bottom />
+        {country && <CountryInfo country={country} />}
+        {error && <Heading title="SearchCountry" bottom />}
+        {loading && <Loader />}
       </Container>
     </Section>
   );
